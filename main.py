3d73d8935ca991067ee9f21597f5f0ade551060b
letiziafaulisi <67644803+letiziafaulisi@users.noreply.github.com>
2022-11-09 21:56:51 +0100

    Upload main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/getRandomNumber')
+def randomNumber():
+    num = str(random.randint(0,900000))
+    #num = str(random.random())
+    output = "Benvenuti!" + "\n" + " Numero random: " + num
+    return output
+
+@app.route('/getSquare/<number>')
+def square(number):
+    try:
+        num = str(number)
+        result = str(int(number) * int(number))
+        return "Il quadrato del numero " + num + " è:  " + result
+    except ValueError:
+        return "Error 200: insert a valid number"
+    except Exception:
+        return "Error 500"
+
+@app.route('/getSquare/')
+def square_nonumber():
+    return "Error 200: missing value"
+
+if __name__ == '__main__':
+    app.run()
